6caf78e5d3da986e761c8a68015727ed6e3b7071
vv <alfervladimir@yandex.ru>
2023-07-16 19:42:39 +0300

    chore: добавил валидацию на дату

--- ewm-main-service/src/main/java/ru/practicum/main/exception/model/EntityNotValidException.java
+++ ewm-main-service/src/main/java/ru/practicum/main/exception/model/EntityNotValidException.java
@@ -2,6 +2,6 @@ package ru.practicum.main.exception.model;
 
 public class EntityNotValidException extends RuntimeException {
     public EntityNotValidException(String entity, Long id) {
-        super(String.format("Entity %s with id %d was not found", entity, id));
+        super(String.format("Entity %s with id %d was invalid", entity, id));
     }
 }
