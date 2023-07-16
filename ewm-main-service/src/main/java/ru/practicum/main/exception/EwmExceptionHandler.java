0a73e6677ff97221b22cb05a101fcfff57fbcaa2
vv <alfervladimir@yandex.ru>
2023-07-16 19:50:01 +0300

    chore: убрал лишний импорт

--- ewm-main-service/src/main/java/ru/practicum/main/exception/EwmExceptionHandler.java
+++ ewm-main-service/src/main/java/ru/practicum/main/exception/EwmExceptionHandler.java
@@ -1,7 +1,6 @@
 package ru.practicum.main.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
