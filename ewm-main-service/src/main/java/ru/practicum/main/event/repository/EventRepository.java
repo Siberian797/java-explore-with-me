7bc1919b01aa4be393c64e399bc7f83e539e401f
vv <alfervladimir@yandex.ru>
2023-07-18 14:46:24 +0300

    chore: реверт репозитория

--- ewm-main-service/src/main/java/ru/practicum/main/event/repository/EventRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/main/event/repository/EventRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.main.event.model.Event;
 import ru.practicum.main.utils.CommonConstants;
 
@@ -14,6 +15,7 @@ import java.util.Optional;
 import java.util.Set;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+    @Query("SELECT COUNT(e) FROM Event e WHERE e.category.id = ?1")
     long countByCategoryId(Long categoryId);
 
     Optional<Event> findByInitiatorIdAndId(Long initiatorId, Long eventId);
@@ -24,22 +26,51 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @EntityGraph(value = Event.GRAPH_EVENT, type = EntityGraph.EntityGraphType.LOAD)
     List<Event> getByInitiatorIdOrderByEventDateDesc(Long id, Pageable pageable);
 
-    @Query("SELECT e FROM Event e where (?1 is null or e.initiator.id in ?1) and (?2 is null or e.state in ?2) " +
-            "and (?3 is null or e.category.id in ?3) and e.eventDate >= ?4 and e.eventDate <= ?5 order by e.id desc ")
+    @Query("SELECT e FROM Event e " +
+            "where ((:users) is null or e.initiator.id in :users) " +
+            "and ((:states) is null or e.state in :states) " +
+            "and ((:categories) is null or e.category.id in :categories) " +
+            "and e.eventDate >= :rangeStart " +
+            "and e.eventDate <= :rangeEnd " +
+            "order by e.id desc ")
     @EntityGraph(value = Event.GRAPH_EVENT, type = EntityGraph.EntityGraphType.LOAD)
-    List<Event> getEventsForAdmin(List<Long> users, List<CommonConstants.EventState> states, List<Long> categories,
-                                  LocalDateTime rangeStart, LocalDateTime rangeEnd, Pageable pageable);
+    List<Event> getEventsForAdmin(
+            @Param("users") List<Long> users,
+            @Param("states") List<CommonConstants.EventState> states,
+            @Param("categories") List<Long> categories,
+            @Param("rangeStart") LocalDateTime rangeStart,
+            @Param("rangeEnd") LocalDateTime rangeEnd,
+            Pageable pageable
+    );
 
-    @Query("SELECT e from Event e where (?1 is null or e.category.id in ?1)")
+    @Query("SELECT e from Event e " +
+            "where ((:categories) is null or e.category.id in :categories)")
     @EntityGraph(value = Event.GRAPH_EVENT, type = EntityGraph.EntityGraphType.LOAD)
-    List<Event> getEventsForUser(List<Long> categories, Pageable pageable);
+    List<Event> getEventsForUser(
+            @Param("categories") List<Long> categories,
+            Pageable pageable
+    );
 
-    @Query("SELECT e from Event e where (UPPER(e.annotation) like UPPER(CONCAT('%',?1,'%')) or UPPER(e.description) " +
-            "like UPPER(CONCAT('%',?1,'%')) or ?1 is null ) and e.state = 'PUBLISHED' and (?2 is null or e.category.id in ?2) " +
-            "and e.paid = ?3 and e.eventDate > ?4 and e.participants.size < e.participantLimit and e.eventDate <= ?5")
+    @Query("SELECT e from Event e " +
+            "where (UPPER(e.annotation) like UPPER(CONCAT('%',:text,'%')) " +
+            "or UPPER(e.description) like UPPER(CONCAT('%',:text,'%')) or :text is null ) " +
+            "and e.state = 'PUBLISHED' " +
+            "and ((:categories) is null or e.category.id in :categories) " +
+            "and e.paid = :paid " +
+            "and e.eventDate > :rangeStart " +
+            "and e.participants.size < e.participantLimit " +
+            "and e.eventDate <= :rangeEnd")
     @EntityGraph(value = Event.GRAPH_EVENT, type = EntityGraph.EntityGraphType.LOAD)
-    List<Event> getAvailableEventsForUser(String text, List<Long> categories, Boolean paid, LocalDateTime rangeStart, LocalDateTime rangeEnd, Pageable pageable);
+    List<Event> getAvailableEventsForUser(
+            @Param("text") String text,
+            @Param("categories") List<Long> categories,
+            @Param("paid") Boolean paid,
+            @Param("rangeStart") LocalDateTime rangeStart,
+            @Param("rangeEnd") LocalDateTime rangeEnd,
+            Pageable pageable
+    );
 
-    @Query("SELECT e from Event e where e.id = ?1 and e.state = 'PUBLISHED'")
-    Optional<Event> findByIdAndPublished(Long eventId);
+
+    @Query("SELECT e from Event e where e.id = :eventId and e.state = 'PUBLISHED'")
+    Optional<Event> findByIdAndPublished(@Param("eventId") Long eventId);
 }
